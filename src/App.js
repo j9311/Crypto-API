6a48a32abd18ea23d1b0e8d829236fd9e4f37db0
James Post <avilapost93@gmail.com>
2021-02-08 11:40:49 -0800

    app js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css';
-import coin from './coin'
+import coin from './coin';
 
 
 function App() {
-const [coins, setCoins] = useState([])
-const [search, setSearch] = useState('')
+let [coins, setCoins] = useState([])
+let [search, setSearch] = useState('')
 useEffect(()=>{
   axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     )
     .then(res => {
       setCoins(res.data)
-      // console.log(res.data)
+      console.log(res.data)
     }).catch(error => console.log('error'))
 }, [])
 
@@ -35,7 +35,7 @@ const filteredCoins = coins.filter(coin =>
     </div>
     {filteredCoins.map(coin => {
       return (
-        <coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.market_cap} />
+        <coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.market_cap} price={coin.current_price}/>
       )
     })}
     </div>
